0942c7380938b30c86e5e02006432949b12662c0
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-09-04 15:00:58 +0300

    button implementation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { clsx } from 'clsx';
 import classes from './Heaer.module.scss';
 import {
   FaFacebookF,
@@ -7,6 +8,8 @@ import {
 } from 'react-icons/fa';
 
 function Header() {
+  const stylesHeaderButton = clsx('btn', classes.header__btn);
+
   return (
     <header className={classes.header}>
       <div className={classes.header__logoWrap}>
@@ -33,9 +36,12 @@ function Header() {
           <a href="#!">Services</a>
           <a href="#!">Contact</a>
         </nav>
-        <a href="#!" className="cartOpenLink">
+        <a href="#!" className="shoppingCartButton">
           <FaShoppingCart />
         </a>
+        <button type="button" className={stylesHeaderButton}>
+          order today
+        </button>
       </div>
     </header>
   );
