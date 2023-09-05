e174db2ae9f7337765edd4dd321dc14ec486901d
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-09-05 12:39:11 +0300

    header implementation for desktop, without droptdown-toggle

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,43 +1,57 @@
 import { clsx } from 'clsx';
-import classes from './Heaer.module.scss';
+import './Header.scss';
 import {
   FaFacebookF,
   FaTwitter,
   FaLinkedinIn,
   FaShoppingCart,
+  FaAngleDown,
 } from 'react-icons/fa';
 
 function Header() {
-  const stylesHeaderButton = clsx('btn', classes.header__btn);
+  const stylesHeaderButton = clsx('btn', 'header__btn');
 
   return (
-    <header className={classes.header}>
-      <div className={classes.header__logoWrap}>
+    <header className={'header'}>
+      <div className={'header__logo-wrap'}>
         <a href="#!" className="logo">
           <img src="./images/logo.svg" alt="logo" />
         </a>
-        <div className={classes.header__socialLinksWrap}>
-          <a href="#!" className="socialLink socialLink_w">
+        <div className={'header__social-links-wrap'}>
+          <a href="#!" className="social-link social-link_w">
             <FaFacebookF />
           </a>
-          <a href="#!" className="socialLink socialLink_w">
+          <a href="#!" className="social-link social-link_w">
             <FaTwitter />
           </a>
-          <a href="#!" className="socialLink socialLink_w">
+          <a href="#!" className="social-link social-link_w">
             <FaLinkedinIn />
           </a>
         </div>
       </div>
-      <div className={classes.header__navbarWrap}>
-        <nav>
-          <a href="#!">Home</a>
-          <a href="#!">Pages</a>
-          <a href="#!">About</a>
-          <a href="#!">Services</a>
-          <a href="#!">Contact</a>
+      <div className={'header__nav-menu-wrap'}>
+        <nav className="nav-menu">
+          <a className="nav-menu__link" href="#!" data-active="true">
+            Home
+          </a>
+          <a className="nav-menu__link" href="#!">
+            About
+          </a>
+          <div className="dropdown-toggle nav-menu__link">
+            <p>Pages</p>
+            <FaAngleDown />
+          </div>
+
+          <a className="nav-menu__link" href="#!">
+            Services
+          </a>
+          <a className="nav-menu__link" href="#!">
+            Contact
+          </a>
         </nav>
-        <a href="#!" className="shoppingCartButton">
+        <a href="#!" className="shopping-cart-button">
           <FaShoppingCart />
+          <div className="shopping-cart-button__count">0</div>
         </a>
         <button type="button" className={stylesHeaderButton}>
           order today
