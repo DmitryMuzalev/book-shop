187d1f28861071067d560029411ffedcfd50ddda
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-09-11 12:37:01 +0300

    adaptive for the AuthorsBook section

--- src/components/AuthorsBook/AuthorsBook.jsx
+++ src/components/AuthorsBook/AuthorsBook.jsx
@@ -1,23 +1,24 @@
-import { SectionTitle } from '../SectionTitle/SectionTitle';
+import "./AuthorsBook.scss";
+import { SectionTitle } from "../SectionTitle/SectionTitle";
 
 const books = [
   {
     id: 1,
-    image: './images/books/book_1.svg',
-    title: 'atomic one’s',
+    image: "./images/books/book_1.svg",
+    title: "atomic one’s",
     price: 13.84,
     annotation:
-      'As the book contains theoretical content as well as solved questions.',
-    format: 'printed book',
+      "As the book contains theoretical content as well as solved questions.",
+    format: "printed book",
   },
   {
     id: 2,
-    image: './images/books/book_2.svg',
-    title: 'the dark light',
+    image: "./images/books/book_2.svg",
+    title: "the dark light",
     price: 86.11,
     annotation:
-      'As the book contains theoretical content as well as solved questions.',
-    format: 'printed book',
+      "As the book contains theoretical content as well as solved questions.",
+    format: "printed book",
   },
 ];
 
@@ -27,17 +28,23 @@ function BookCollectionItem({ image, title, price, annotation, format }) {
       <a href="#!" className="book-collection-item__image">
         <img src={image} alt={title} />
       </a>
-      <div className="book-collection-item__detail">
-        <a href="#!" className="book-collection-item__title">
-          {title}
-        </a>
-        <div className="book-collection-item__price">{`$ ${price} USD`}</div>
-        <p className="book-collection-item__annotation">{annotation}</p>
-        <div className="book-collection-item__format">
-          <span className="marker"></span>
-          <h3>{format}</h3>
+      <div className="book-collection-item__listing">
+        <div className="book-collection-item__detail">
+          <a href="#!" className="book-collection-item__title">
+            {title}
+          </a>
+          <div className="book-collection-item__price">{`$ ${price} USD`}</div>
         </div>
-        <button className="btn btn_transparent">Order Now</button>
+        <div className="book-collection-item__content">
+          <p className="book-collection-item__annotation">{annotation}</p>
+          <div className="book-collection-item__format">
+            <span className="marker"></span>
+            <h3>{format}</h3>
+          </div>
+          <button className="btn btn_transparent book-collection-item__btn">
+            Order Now
+          </button>
+        </div>
       </div>
     </div>
   );
@@ -49,7 +56,7 @@ function AuthorsBook() {
       <div className="container">
         <div className="authors-book__wrap">
           <SectionTitle>the author’s book</SectionTitle>
-          <ul className="book-collection">
+          <ul className="book-collection authors-book__content">
             {books.map((book) => {
               return (
                 <li key={book.id}>
