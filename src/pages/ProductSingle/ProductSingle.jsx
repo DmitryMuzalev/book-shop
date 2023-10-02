c36a4b8bb0fc8987f3731a6abfc27cb3b5187344
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-10-02 15:40:30 +0300

    adaptive product-single-page and shop-trust

--- src/pages/ProductSingle/ProductSingle.jsx
+++ src/pages/ProductSingle/ProductSingle.jsx
@@ -1,33 +1,37 @@
-import { ProductInformation } from './ProductInformation';
-import { ProductSingleCart } from './ProductSingleCart';
-import { ProductSingleParameters } from './ProductSingleParameters';
+import { ProductInformation } from "./ProductInformation";
+import { ProductSingleCart } from "./ProductSingleCart";
+import { ProductSingleParameters } from "./ProductSingleParameters";
+import { ShopTrust } from "./ShopTrust";
 
 function ProductSingle() {
   return (
-    <section className="product-single section-wrap">
-      <div className="container">
-        <div className="product-single__wrap">
-          <div className="product-single__image-wrap">
-            <div className="product-single__image">
-              <img src="../images/books/book_1.svg" alt="book_1" />
+    <>
+      <section className="product-single section-wrap">
+        <div className="container">
+          <div className="product-single__wrap">
+            <div className="product-single__image-wrap">
+              <div className="product-single__image">
+                <img src="../images/books/book_1.svg" alt="book_1" />
+              </div>
             </div>
-          </div>
-          <div className="product-single__detail">
-            <h2 className="product-single__title">Atomic One’s - Audio</h2>
-            <div className="product-single__cost">$ 68.97 USD</div>
-            <div className="product-single__description paragraph">
-              The book contains ample theoretical content as well as a number of
-              solved questions, it for sure will help aspirants succeed in
-              learning and writing English proficiently.
-            </div>
+            <div className="product-single__detail">
+              <h2 className="product-single__title">Atomic One’s - Audio</h2>
+              <div className="product-single__cost">$ 68.97 USD</div>
+              <div className="product-single__description paragraph">
+                The book contains ample theoretical content as well as a number
+                of solved questions, it for sure will help aspirants succeed in
+                learning and writing English proficiently.
+              </div>
 
-            <ProductSingleParameters />
-            <ProductSingleCart />
+              <ProductSingleParameters />
+              <ProductSingleCart />
+            </div>
           </div>
+          <ProductInformation />
         </div>
-        <ProductInformation />
-      </div>
-    </section>
+      </section>
+      <ShopTrust />
+    </>
   );
 }
 export { ProductSingle };
